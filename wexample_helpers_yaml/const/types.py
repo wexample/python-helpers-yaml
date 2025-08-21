de9102a539efb01460dccad8ad1da2bb885c4748
weeger <romain.weeger@gmail.com>
2025-08-21 18:43:46 +0200

    Sort imports

--- wexample_helpers_yaml/const/types.py
+++ wexample_helpers_yaml/const/types.py
@@ -1,6 +1,6 @@
 from typing import TypeAlias
 
-from wexample_helpers.const.types import StructuredData, StringKeysDict
+from wexample_helpers.const.types import StringKeysDict, StructuredData
 
 YamlContent: TypeAlias = StructuredData
 YamlContentDict = StringKeysDict
